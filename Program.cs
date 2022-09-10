2a0d7165a8907756b385940cf571d099c6729014
Tosh <88.a.potapov@gmail.com>
2022-09-11 01:59:59 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Write("Какое количество выражений вы планируете? Введите число: ");
+int Length1 = int.Parse(Console.ReadLine());
+string[] arr1 = new string [Length1];
+int Length2 = 0;
+for(int i = 0; i < Length1; i++)
+{
+    Console.Write("Введите выражение: ");
+    arr1[i] = Console.ReadLine();
+    if (arr1[i].Length <= 3) Length2++;
+}
